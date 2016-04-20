92ec702478477182b03b1f2b364a08f0dc354658
Satcha Villafuerte <svillafuerte@growthaccelerationpartners.com>
2016-04-20 10:23:23 -0600

    Adding new on-premise api module

--- app/modules/on_prem_api.rb
+++
@@ -1,8 +0,0 @@
-module OnPremApi
-
-  def request_api(endpoint, method, parameters, config)
-  	RestClient::Request.execute(:url => "#{config.kube[:url]}/#{endpoint}", 
-  								:method => method, :payload => parameters, accept: :json, content_type: :json)
-  end
-  
-end

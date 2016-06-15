437f7c4583e9770918c023916f426e05acdedbd2
Satcha Villafuerte <svillafuerte@growthaccelerationpartners.com>
2016-06-15 10:59:36 -0600

    Remove unnecesary constructor for exception class

--- app/modules/exceptions.rb
+++ app/modules/exceptions.rb
@@ -1,8 +1,5 @@
 # This class is responsible for handle exceptions for collector
 module Exceptions
   class CollectorException < StandardError
-    def initialize(message)
-      @message = message
-    end
   end
 end

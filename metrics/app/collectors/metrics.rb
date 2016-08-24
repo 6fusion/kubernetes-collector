44e5ee0ed65231ace5244b2fbcfe5af79a80e6fb
Delano Seymour <dseymour@6fusion.com>
2016-08-24 14:23:13 -0300

    Fixed issues when cadvisor does not have metric data.

--- metrics/app/collectors/metrics.rb
+++ metrics/app/collectors/metrics.rb
@@ -32,31 +32,41 @@ class MetricsCollector
         if previous_sample
           machine_sample = machine.machine_samples.new(reading_at: sample['timestamp'])
           if sample['has_cpu']
-            machine_sample.cpu_usage_percent = ((sample['cpu']['usage']['total'] - previous_sample['cpu_usage']).abs / (1000000000.0 * machine.cpu_count)) * 100
-          end unless sample['cpu'].empty?
+            unless sample['cpu'].empty?
+              machine_sample.cpu_usage_percent = ((sample['cpu']['usage']['total'] - previous_sample['cpu_usage']).abs / (1000000000.0 * machine.cpu_count)) * 100
+            end
+          end 
           if sample['has_memory']
-            machine_sample.memory_bytes = sample['memory']['usage']
-          end unless sample['memory'].empty?
+            unless sample['memory'].empty?
+              machine_sample.memory_bytes = sample['memory']['usage']
+            end
+          end 
           machine_sample.save!
 
           disk_sample = machine.disks.first.disk_samples.new(reading_at: sample['timestamp'])
           if sample['has_diskio']
-            disk_sample.reading_at = sample['timestamp']
-            disk_sample.read_kilobytes = (sample['diskio']['io_service_bytes'][0]['stats']['Read'] - previous_sample['diskio_bytes_read']).abs / 1024
-            disk_sample.write_kilobytes = (sample['diskio']['io_service_bytes'][0]['stats']['Write'] - previous_sample['diskio_bytes_write']).abs / 1024
-          end unless sample['diskio'].empty?
+            unless sample['diskio'].empty?
+              disk_sample.reading_at = sample['timestamp']
+              disk_sample.read_kilobytes = (sample['diskio']['io_service_bytes'][0]['stats']['Read'] - previous_sample['diskio_bytes_read']).abs / 1024
+              disk_sample.write_kilobytes = (sample['diskio']['io_service_bytes'][0]['stats']['Write'] - previous_sample['diskio_bytes_write']).abs / 1024
+            end
+          end 
           if sample["has_filesystem"]
-            disk_sample.usage_bytes = sample["filesystem"][0]["usage"]
-          end unless sample["filesystem"].empty?
+            unless sample["filesystem"].empty?
+              disk_sample.usage_bytes = sample["filesystem"][0]["usage"]
+            end
+          end 
           disk_sample.save!
 
           if sample['has_network']
-            nic_sample = machine.nics.first.nic_samples.new(reading_at: sample['timestamp'])
-            nic_sample.reading_at = sample['timestamp']
-            nic_sample.receive_kilobits = (sample['network']['interfaces'][0]['rx_bytes'] - previous_sample['network_bytes_receive']) * 8 / 1000
-            nic_sample.transmit_kilobits = (sample['network']['interfaces'][0]['tx_bytes'] - previous_sample["network_bytes_transmit"]) * 8 / 1000
-            nic_sample.save!
-          end unless sample['network'].empty?
+            unless sample['network'].empty?
+              nic_sample = machine.nics.first.nic_samples.new(reading_at: sample['timestamp'])
+              nic_sample.reading_at = sample['timestamp']
+              nic_sample.receive_kilobits = (sample['network']['interfaces'][0]['rx_bytes'] - previous_sample['network_bytes_receive']) * 8 / 1000
+              nic_sample.transmit_kilobits = (sample['network']['interfaces'][0]['tx_bytes'] - previous_sample["network_bytes_transmit"]) * 8 / 1000
+              nic_sample.save!
+            end
+          end 
         end
 
         previous_sample = {}
@@ -67,20 +77,26 @@ class MetricsCollector
         previous_sample['network_bytes_transmit'] = 0
 
         if sample['has_cpu']
-          previous_sample['cpu_usage'] = sample['cpu']['usage']['total']
-        end unless sample['cpu'].empty?
+          unless sample['cpu'].empty?
+            previous_sample['cpu_usage'] = sample['cpu']['usage']['total']
+          end
+        end 
 
         if sample['has_diskio']
-          previous_sample['diskio_bytes_read'] = sample['diskio']['io_service_bytes'][0]['stats']['Read']
-          previous_sample['diskio_bytes_write'] = sample['diskio']['io_service_bytes'][0]['stats']['Write']
-        end unless sample['diskio'].empty?
+          unless sample['diskio'].empty?
+            previous_sample['diskio_bytes_read'] = sample['diskio']['io_service_bytes'][0]['stats']['Read']
+            previous_sample['diskio_bytes_write'] = sample['diskio']['io_service_bytes'][0]['stats']['Write']
+          end
+        end 
 
         if sample['has_network']
-          previous_sample['network_bytes_receive'] = sample['network']['interfaces'][0]['rx_bytes']
-          previous_sample['network_bytes_transmit'] = sample['network']['interfaces'][0]['tx_bytes']
-        end unless sample['network'].empty?
+          unless sample['network'].empty?
+            previous_sample['network_bytes_receive'] = sample['network']['interfaces'][0]['rx_bytes']
+            previous_sample['network_bytes_transmit'] = sample['network']['interfaces'][0]['tx_bytes']
+          end
+        end 
       end if samples
-      
+
       machine.locked = false
       machine.locked_by = ''
       machine.metering_status = 'METERED'
